e0f97ed1611a2543b4b5b0ff7fab15ff782f430a
CyberDie22 <ben@benbuzard.com>
2023-04-06 01:29:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# svelte-headlessui-temp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-wqzq5v)
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+	import './styles.css';
+</script>
+
+<div class="app h-full w-full">
+	<slot />
+</div>
+
+<style>
+</style>
